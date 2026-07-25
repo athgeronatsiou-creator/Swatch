2d542ae84926964c667f114f74ba35225395cbcd
athinager <geronatsiou@thinkdesquared.com>
2026-07-25 18:23:19 +0300

    Add Xcode project for Swatch

---
+++ Swatch/Swatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swatch
+//
+//  Created by Athina Geronatsiou on 25/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swatch/Swatch/SwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwatchApp.swift
+//  Swatch
+//
+//  Created by Athina Geronatsiou on 25/7/26.
+//
+
+import SwiftUI
+
+@main
+struct SwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
