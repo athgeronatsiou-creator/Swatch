71e035d83238edba8138f4167c22092832d0c705
athinager <geronatsiou@thinkdesquared.com>
2026-07-25 18:35:15 +0300

    Change icon and greeting text.

--- Swatch/Swatch/ContentView.swift
+++ Swatch/Swatch/ContentView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, girlies!")
+            Text("Hey girlies!")
         }
-        .padding()
+        .padding(30)
     }
 }
 
